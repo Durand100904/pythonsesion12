3fa9eab78bb65af8c51beb3b7243164deb64db12
Durand100904 <PI75279857@zegelipae.pe>
2022-07-25 23:12:49 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("ingrese el primer numero")
+n1 = float(input())
+print("ingrese al segundo numero")
+n2 = float(input())
+suma = n1 + n2
+resta = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+print("la suma es: ", suma )
+print("la resta es:" , resta)
+print("la multiplicacion es:", mult)
+print("la division es:", div)
